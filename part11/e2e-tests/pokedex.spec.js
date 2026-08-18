98c82cb41d1e7e65226f4f689666d21228e6f45b
John Ayodele <johnayodele095@gmail.com>
2026-08-18 06:01:42 +0000

    git deploy

---
+++ part11/e2e-tests/pokedex.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('/')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(
+      page.getByText(
+        'Pokémon and Pokémon character names are trademarks of Nintendo.'
+      )
+    ).toBeVisible()
+  })
+
+  test('can navigate to a Pokemon page', async ({ page }) => {
+    await page.goto('/')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
